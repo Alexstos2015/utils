ae1d685fcd536bc26ce638d4b9f2fc015b9aa18f
Alexstos2015 <alexezhov169@gmail.com>
2017-08-30 16:23:22 +0300

    FIrst Step: collect scripts into one file

--- sqlScriptCollect.py
+++ sqlScriptCollect.py
@@ -1,2 +1,17 @@
-''' Тестовая запись
+import os,sys
+
+scriptFiles = filter(os.path.isfile, os.listdir('./scripts'))
+
+print(list(scriptFiles))
+
+resFile = open('BSSRP.sql','w')
+
+for i in list(scriptFiles):
+    currentScript = open(i,'r')
+    content = currentScript.read()
+    currentScript.close()
+    resFile.write('-- '+i+'\n')
+    resFile.write(content)
+
+resFile.close
 
